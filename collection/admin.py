b1296ee3d9996fd1f28cd055502a563a8250bf38
seenaomi <naomi.see@seenaomi.net>
2016-11-26 13:32:44 -0600

    Admin login and portion of site created. DB created.

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from collection.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+	model = Post
+	list_display = ('title', 'content',)
+	prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
