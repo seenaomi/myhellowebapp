84883cf07611f2c81e175760d429469673555c13
seenaomi <naomi.see@seenaomi.net>
2016-11-26 00:03:03 -0600

    adding template content

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	# defining the variable
+	number = 6
+	# don't forget the quotes because it's a string, not an integer
+	thing = "Thing name"
 	# this is your new view
-	return render(request, 'index.html')
+	return render(request, 'index.html', {
+		'number': number,
+		'thing': thing,
+	})
