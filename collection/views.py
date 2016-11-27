b105eef8c6f36d484cc16a656fee02696ead617c
seenaomi <naomi.see@seenaomi.net>
2016-11-26 19:13:04 -0600

    updating files to have DB data display on home page

--- collection/views.py
+++ collection/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
+from collection.models import Post
 
 # Create your views here.
 def index(request):
-	# defining the variable
-	number = 6
-	# don't forget the quotes because it's a string, not an integer
-	thing = "Thing name"
-	# this is your new view
+	posts = Post.objects.all()
 	return render(request, 'index.html', {
-		'number': number,
-		'thing': thing,
-	})
+		'posts': posts,
+		})
